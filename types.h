dde774d116f70dfef9ffc13b3c638d5881d38102
joH1 <jiema@live.fr>
2017-12-01 09:44:44 +0100

    removed macros (moved to math.h), added docs

--- types.h
+++ types.h
@@ -1,33 +1,40 @@
+/**
+ * \file types.h
+ * \author joH1
+ *
+ * This file contains several definitions of types used throughout the whole
+ * project.
+*/
+
 #ifndef TYPES_H
 #define TYPES_H
 
 #include <stdio.h>
-
 #include <SDL2/SDL.h>
+
 
 /* ## SDL-related types ## */
 
-
-typedef SDL_Renderer Renderer;
+typedef SDL_Renderer Renderer; /**< The rendering structure */
 typedef SDL_RendererFlags RenderFlags;
-typedef SDL_WindowFlags WinFlags;
+/**< The flags used to initialize the renderer */
+typedef SDL_WindowFlags WinFlags; /**< The flags to initialize the window */
 
-typedef SDL_Color Color;
+typedef SDL_Color Color; /**< A RGBA color structure */
 
-typedef SDL_Point Point;
+typedef SDL_Point Point; /**< A geometric point, in cartesian coordinates */
 
-typedef SDL_Rect Rect;
+typedef SDL_Rect Rect; /**< A geometric rectangle */
 
-
-#define point(x, y) ((Point){(x), (y)})
-#define rect(x, y, w, h) ((Rect){(x), (y), (w), (h)})
 
 
 /* ## Other uselful types ## */
 
-typedef char *str;
-typedef const char *cstr;
-typedef FILE *File;
+/* TODO make sure these are useful */
+typedef char *str; /**< A simple redefinition of a string */
+typedef const char *cstr; /**< A redefinition for constant srings */
+
+typedef FILE *File; /**< A redefinition of the file structure */
 
 
 #endif /* TYPES_H */
