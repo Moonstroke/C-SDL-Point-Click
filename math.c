c9a4ee7e5e9a76eafd123558fa083b12ada71fec
joH1 <jiema@live.fr>
2017-12-01 09:35:54 +0100

    added point() and rect() functions

--- math.c
+++ math.c
@@ -3,6 +3,10 @@
 
 #include <math.h>
 
+extern Point point(int x, int y);
+
+extern Rect rect(int x, int y, unsigned int w, unsigned int h);
+
 
 double distance(const Point p1, const Point p2) {
 	return sqrt(pow(p1.x - p2.x, 2) + pow(p1.x - p2.x, 2));
--- math.h
+++ math.h
@@ -1,17 +1,60 @@
+/**
+ * \file math.h
+ * \author joH1
+ *
+ * This file contains every function used in the project that involves
+ * mathematical calculations.
+*/
+
 #ifndef MATH_H
 #define MATH_H
 
 #include "types.h"
 
 
+/**
+ \brief The maximum distance at which a mouseup following a mousedown is
+ *      considered a mouse click.
+ */
 #define MAX_CLICK_DISTANCE 0.1
 
 /**
  * \brief Calculates the distance between two geometric points.
  *
+ * \param[in] p1 A -- valid -- point in the plane
+ * \param[in] p2 Another point in the plane
+ *
  * \return The quadratic distance between the two points
  */
 double distance(Point p1, Point p2);
 
+/**
+ * \brief Constructs a geometric point, in cartesian coordinates.
+ *
+ * \param[in] x The abscissa of the point
+ * \param[in] y The ordinate of the point
+ *
+ * \return A Point structure element
+ *
+ * \sa Point
+ */
+inline Point point(const int x, const int y) {
+	return (Point){.x = x, .y = y};
+}
 
+/**
+ * \brief Constructs a geometric rectangle.
+ *
+ * \param[in] x The abscissa of the rectangle
+ * \param[in] y The ordinate of the rectangle
+ * \param[in] w The width of the rectangle
+ * \param[in] h The height of the rectangle
+ *
+ * \return A Rect structure object
+ *
+ * \sa Rect
+ */
+inline Rect rect(const int x, const int y, const unsigned int w, const unsigned int h) {
+	return (Rect){.x = x, .y = y, .w = w, .h = h};
+}
 #endif /* MATH_H */
