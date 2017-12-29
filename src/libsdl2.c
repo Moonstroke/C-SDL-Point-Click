f7e36b7d5c457020e45a63db7df80d99f4372337
joH1 <jiema@live.fr>
2017-12-29 19:16:11 +0100

    Rewrite logSDLRendererInfo()
    The function logSDLRendererInfo() works but can use rewriting.
    Add preliminary `"Renderer infos"` message with initial newline, and concluding
    newline to better delimitate the messages block that constitutes the logging of
    the renderer infos.
    Add `0x` prefixes to number output in hexadecimal.
    Change the names of the variables: *reninfo* -> *i* and *i* -> *k*.
    Change the memory width of variables: *n* and *i* are now `char`s rather than `int`s
    (since they are certain to be <= 16).

--- src/libsdl2.c
+++ src/libsdl2.c
@@ -37,14 +37,16 @@ void clearSDL(void) {
 }
 
 void logSDLRendererInfo(SDL_Renderer *const ren) {
-	SDL_RendererInfo reninfo;
-	if(SDL_GetRendererInfo(ren, &reninfo) < 0) return;
-	info("Renderer name:  \"%s\"\n", reninfo.name);
-	info("Renderer flags: %x\n", reninfo.flags);
-	const int n = reninfo.num_texture_formats;
-	info("# of available texture formats: %d", n);
-	int i;
-	for(i = 0; i < n; ++i)
-		info("Texture format #%2d:           %x", i, reninfo.texture_formats[i]);
-	info("Maximum texture dimension:      (%d x %d)", reninfo.max_texture_width, reninfo.max_texture_height);
+	SDL_RendererInfo i;
+	if(SDL_GetRendererInfo(ren, &i) < 0)
+		return;
+	info("\nRenderer infos");
+	info("Renderer name: \"%s\"", i.name);
+	info("Renderer flags: 0x%x", i.flags);
+	const unsigned char n = i.num_texture_formats;
+	info("%u available texture formats", n);
+	for(unsigned char k = 0; k < n; ++k)
+		info("Texture format #%u: 0x%x", k + 1, i.texture_formats[k]);
+	info("Maximum texture dimension: (%u x %u)\n", i.max_texture_width,
+	                                               i.max_texture_height);
 }
