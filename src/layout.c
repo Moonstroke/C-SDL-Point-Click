74adb3096140593450a3cd4296fe886b88e6da8b
joH1 <jiema@live.fr>
2017-12-30 19:25:07 +0100

    Revise control flow in setLayout().
    The function now directly returns from the `switch` on nominal ending,
    and the `default` case falls back to the final `return` statement.

--- src/layout.c
+++ src/layout.c
@@ -7,7 +7,7 @@ bool setLayout(const Window *const w, const LayoutValues l, Rect *const sg, Rect
 		case SCENE_ONLY:
 			ig->x = ig->y = ig->w = ig->h = 0;
 			sg->x = sg->y = 0, sg->w = winw, sg->h = winh;
-			break;
+			return true;
 		case SCENE_INVENTORY_ABOVE:
 			// TODO
 			return false;
@@ -16,18 +16,16 @@ bool setLayout(const Window *const w, const LayoutValues l, Rect *const sg, Rect
 				return false;
 			ig->x = 0, ig->y = winh - d, ig->w = winw, ig->h = d;
 			sg->x = sg->y = 0, sg->w = winw, sg->h = winw - d;
-			break;
+			return true;
 		case SCENE_INVENTORY_ON_LEFT:
 			if(d > winw)
 				return false;
 			ig->x = ig->y = 0, ig->w = d, ig->h = winh;
 			sg->x = d, sg->y = 0, sg->w = winw - d, sg->h = winh;
-			break;
+			return true;
 		case SCENE_INVENTORY_ON_RIGHT:
 			// TODO
 			return false;
-		default:
-			return false;
 	}
-	return true;
+	return false;
 }
