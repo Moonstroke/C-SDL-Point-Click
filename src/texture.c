5eb57feadca21efb36948ee22a0d82e9dd8c593e
joH1 <jiema@live.fr>
2017-12-30 18:49:43 +0100

    Shorten parameters names in Texture functions
    The names are now one-character long, everywhere in `texture.c` where it does not
    hinder reading.

--- src/texture.c
+++ src/texture.c
@@ -8,15 +8,15 @@
 
 
 
-Texture *loadBMPA(const str filename, const Window *const win, uint32_t colorkey) {
-	SDL_Surface *bmp = SDL_LoadBMP(filename);
+Texture *loadBMPA(const str f, const Window *const w, uint32_t c) {
+	SDL_Surface *bmp = SDL_LoadBMP(f);
 	if(!bmp) {
-		error("Could not load BMP file \"%s\": %s", filename, SDL_GetError());
+		error("Could not load BMP file \"%s\": %s", f, SDL_GetError());
 		return NULL;
 	}
 	if(colorkey)
-		SDL_SetColorKey(bmp, true, colorkey); // FIXME
-	Texture *tex = SDL_CreateTextureFromSurface(getWindowRenderer(win), bmp);
+		SDL_SetColorKey(bmp, true, c); // FIXME
+	Texture *tex = SDL_CreateTextureFromSurface(getWindowRenderer(w), bmp);
 	SDL_FreeSurface(bmp); // TODO make sure this won't call SDL_SetError()
 	if(!tex) {
 		error("Could not load Texture from Surface: %s", SDL_GetError());
@@ -24,17 +24,17 @@ Texture *loadBMPA(const str filename, const Window *const win, uint32_t colorkey
 	}
 	return tex;
 }
-extern Texture *loadBMP(str filename, const Window *win);
+extern Texture *loadBMP(str f, const Window *w);
 
 
 void freeTexture(Texture *const t) {
 	SDL_DestroyTexture(t);
 }
 
-void drawTexture(Texture *const t, Window *const win, const Point pos) {
-	Rect r = {.x = pos.x, .y = pos.y};
+void drawTexture(Texture *const t, Window *const w, const Point p) {
+	Rect r = {.x = p.x, .y = p.y};
 	SDL_QueryTexture(t, NULL, NULL, &r.w, &r.h);
-	SDL_RenderCopy(getWindowRenderer(win), t, NULL, &r);
+	SDL_RenderCopy(getWindowRenderer(w), t, NULL, &r);
 }
 
 bool getTextureGeom(const Texture *const t, int *w, int *h) {
