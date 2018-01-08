4c01c92f9c94ed5d7aa61b712d6fa0756827043d
joH1 <jiema@live.fr>
2018-01-09 00:11:37 +0100

    Remove comments

--- src/layout.c
+++ src/layout.c
@@ -16,14 +16,14 @@ bool setLayout(const Window *const w, const LayoutValues l, Rect *const sg, Rect
 		case SCENE_INVENTORY_BELOW:
 			if(d > winh)
 				return false;
-			*ig = rect(winw, d, 0, winh - d); //ig->x = 0, ig->y = winh - d, ig->w = winw, ig->h = d;
-			*sg = rect(winw, winw - d, 0, 0); //sg->x = sg->y = 0, sg->w = winw, sg->h = winw - d;
+			*ig = rect(winw, d, 0, winh - d);
+			*sg = rect(winw, winh - d, 0, 0);
 			return true;
 		case SCENE_INVENTORY_ON_LEFT:
 			if(d > winw)
 				return false;
-			*ig = rect(d, winh, 0, 0); //ig->x = ig->y = 0, ig->w = d, ig->h = winh;
-			*sg = rect(winw - d, winh, 0, 0); //sg->x = d, sg->y = 0, sg->w = winw - d, sg->h = winh;
+			*ig = rect(d, winh, 0, 0);
+			*sg = rect(winw - d, winh, 0, 0);
 			return true;
 		case SCENE_INVENTORY_ON_RIGHT:
 			// TODO
